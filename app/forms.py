fd35a11b64353bccfc1fb833dce31e3e2e7f6898
ocmv <orianacmv@gmail.com>
2021-02-27 21:23:20 -0400

    add validations

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,20 @@
-from wtforms import Form
+from wtforms import Form,  HiddenField
 from wtforms import validators
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.fields.html5 import EmailField
+from .models import User
+#validacion nombre usuario no permitido
+def admin_validator(form, field):
+	if field.data == 'admin' or field.data == 'admin':
+		raise validators.ValidationError('El username admin no es permitido')
+
+#validacion honeypot
+def length_honeypot(form, field):
+    if len(field.data) > 0:
+        raise validators.ValidationError('Solo los humanos pueden completar el registro!')
 class LoginForm(Form):
 	username = StringField('Username', [
-		validators.length(min=4, max=50, message='El username se encuantra fuera de rango')
+		validators.length(min=4, max=50, message='El username se encuantra fuera de rango'),
 	])
 
 	password = PasswordField('Password', [
@@ -13,7 +23,8 @@ class LoginForm(Form):
 
 class RegisterForm(Form):
 	username = StringField('Username', [
-		validators.length(min=4, max=50)
+		validators.length(min=4, max=50),
+		admin_validator
 	])
 
 	email = EmailField('Correo electronico', [
@@ -32,3 +43,24 @@ class RegisterForm(Form):
 	accept = BooleanField('', [
 		validators.DataRequired()
 	])
+
+	honeypot = HiddenField("", [ length_honeypot] )
+
+	#validacion de campos: validate_campoavalidar
+	def validate_username(self, username):
+		if User.get_by_username(username.data):
+			raise validators.ValidationError('El username ya se encuentra en uso')
+	
+	def validate_email(self, email):
+		if User.get_by_email(email.data):
+			raise validators.ValidationError('El email ya se encuentra en uso')
+
+	#para sobreescribir metodo validate
+	def validate(self):
+		if not Form.validate(self):
+			return False
+
+		if len(self.password.data) < 6:
+			self.password.errors.append('El password es demasiado corto')
+			return False
+		return True
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ import datetime
 from werkzeug.security import generate_password_hash
 from . import db
 
+#clase usuario
 class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
@@ -14,16 +15,27 @@ class User(db.Model):
 	def password(self):
 		pass
 
+	#agregar hash para el password
 	@password.setter
 	def password(self, value):
 		self.encrypted_password = generate_password_hash(value)
 
+	#se crea elemento en la db
 	@classmethod
 	def create_element(cls, username, password, email):
 		user = User(username=username, password=password, email=email)
 		db.session.add(user)
 		db.session.commit()
-
 		return user
+
+	#validaciones campos unicos en db
+	@classmethod
+	def get_by_username(cls, username):
+		return User.query.filter_by(username=username).first()
+	
+	@classmethod
+	def get_by_email(cls, email):
+		return User.query.filter_by(email=email).first()
+
 
 	
